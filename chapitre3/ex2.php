8627348a3b04a00e907c9bddb09be0caf5155cf1
chloe <chloe.duay@cpnv.ch>
2015-04-23 11:26:37 +0200

    exercice3

---
+++ chapitre3/ex2.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Exercice 02</title>
+    <meta charset="utf-8">
+  </head>
+    <body>
+    if (<condition>) {
+    
+    }else{
+        
+    }
+<?php
+for ($i=1;$i<=20;$i++)
+{
+$multiple = $i*2;
+echo "$i * 2 = $multiple <BR>";
+};
+?> // pour 2
+
+<?php
+$j=2;
+while ($j<=20)
+{
+$carre=$j*$j;
+echo "$j ² = $carre<br>";
+$j+=2;
+}// fin du bloc de la boucle
+?>
+
+    </body>
+</html> 
+
