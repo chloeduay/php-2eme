97e00ad76384265f7ace353292c4e9838f6a9369
chloe <chloe.duay@cpnv.ch>
2015-04-23 09:17:38 +0200

    message

--- chapitre2/ex5/devine.php
+++ chapitre2/ex5/devine.php
@@ -1,23 +1,27 @@
+<?php
+$nombre= 0;
+const NBR_MAX = '5';
+const NBR_MIN = '1';
+
+$nombre= rand (NBR_MIN, NBR_MAX);
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-    <title>jeu de devinette</title>
+    <title>Exercice 05</title>
     <meta charset="utf-8">
   </head>
     <body>
-    <h1>jeu de devinette</h1>
-    <form name="resultat" action="resultat.php" method="post">
-        <div>
-            <label for="devine">deviner le nombre entre 0 et 5</label>
-            <input type="text" name="devine" id="devine" />
-        </div>
-        <input type="submit" value="tester" name="tester">
-    </form>
-    <?php
-    $nombre='';
-    $nbr_min= 0;
-    $nbre_max= 5;
-    ?>
-        
+    <h3>jeu de devinette</h3>
+    <p>Deviner le nombre entre 0 et 5</p>
+    <form name="form1" method="post" action="resultat.php" >
+        <input type="text" name="number"/>
+        <p>
+        <input type="submit" value="tester"/>
+        <br>
+        </p>
+        <input type="hidden"
+                name="ValeurADeviner"
+                value"<?= $nombre ?>"
     </body>
 </html> 
--- chapitre2/ex5/resultat.php
+++ chapitre2/ex5/resultat.php
@@ -1,10 +1,40 @@
+<?php
+
+$valeurADeviner =(int) $POST ['valeurADeviner'];
+$valeurEntree = (int) $POST ['nbr'];
+
+$gagne = $valeurADeviner == $ValeurEntree;
+
+if ($gagne){
+echo "You win";
+}else{
+$titre = "You loose";
+
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>exercice 5</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <a href="devine.php?nom=Totoget"> Aller vers devine.php</a>
-  </body>
-</html> 
+    <head>
+        <title> <?= $titre?></title>
+        <meta content="">
+        
+            <?php
+            if ($gagne){
+                $titre = "You win";
+            }else{
+                $titre = "You loose";
+                ?>
+    </head>
+    <body>
+        <?php
+        if ($gagne){
+            echo "<h1> Vous avez gagné </h1>";
+            }else{
+?>
+<h1> Vous avez perdu</h1>
+<a href="javascript:history.back()">réesayer/a>
+<?php
+}
+?>
+    </body>
+</html>
