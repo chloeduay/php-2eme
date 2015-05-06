9181439026e8616a1ae081cac2df2235657d7382
chloe <chloe.duay@cpnv.ch>
2015-05-06 12:41:03 +0200

    test

--- testphp/resultat-test.php
+++ testphp/resultat-test.php
@@ -1,62 +1,51 @@
-<table border=5> 
+<table border="5"> 
 <?php
+
 $nombre= $_POST['nombre']; // Variables
 
 echo "<caption>Table de multiplication en base $nombre</caption>"; // Titre du tableau
 echo "<tr><th>x</th>"; // Cellule haut gauche
 
-for($i=1;$i<=$nombre;$i++)
+for($i=0;$i<$nombre;$i++)
 	echo "<th>$i</th>";
-	echo "</tr>\n";
 
-	for($j=1;$j<=$nombre;$j++)
+echo "</tr>\n";
+
+for($j=0;$j<$nombre;$j++)
+{
+ 	echo "<tr><th>$j</th>";
+
+	for($i=0;$i<$nombre;$i++)
 	{
- 	 	echo "<tr><th>$j</th>";
-
- 		 for($i=1;$i<=$nombre;$i++)
-  	{
-    	echo "<td>".($j*$i)."</td>"; // Affichage dans les cellules
-  }
-}
+		 echo "<td>".base_convert($j*$i, 10, $nombre)."</td>"; // Affichage dans les cellules
+	}
+	
+	echo "</tr>";
+ }
 ?>
 </table>
 <br>
-<table border=5> 
+<table border="5"> 
 <?php
 echo "<caption>Table d'addition en base $nombre</caption>"; // Titre du tableau
 echo "<tr><th>+</th>"; // Cellule haut gauche
 
-for($i=1;$i<=$nombre;$i++)
+for($i=0;$i<$nombre;$i++)
 	echo "<th>$i</th>";
-	echo "</tr>\n";
 
-	for($j=1;$j<=$nombre;$j++)
+echo "</tr>\n";
+
+for($j=0;$j<$nombre;$j++)
+{
+ 	echo "<tr><th>$j</th>";
+
+	for($i=0;$i<$nombre;$i++)
 	{
-	  echo "<tr><th>$j</th>";
-
-		  for($i=1;$i<=$nombre;$i++)
-		  {
-		    echo "<td>".($j+$i)."</td>"; // Affichage dans les cellules
-		  }
-}
+		 echo "<td>".base_convert($j+$i, 10, $nombre)."</td>"; // Affichage dans les cellules
+	}
+	
+	echo "</tr>";
+ }
 ?>
-
-  <thead> 
-    <tr style="background-color:lightgrey;"> 
-      <th style="width:120px;">Titre 1</th> 
-      <th style="width:120px;">Titre 2</th> 
-      <th style="width:18px;"></th> 
-    </tr> 
-  </head> 
-  <tfoot> 
-    <tr style="background-color:lightgrey; text-align:center;"> 
-      <td></td> 
-    </tr> 
-  </tfoot> 
-  <tbody> 
-    <tr> 
-        </div> 
-      </td> 
-    </tr> 
-  </tbody> 
 </table>
+
