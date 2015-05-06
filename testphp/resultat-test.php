e1344ced3d33f4df532b952c174737bd0ef1df95
chloe <chloe.duay@cpnv.ch>
2015-05-06 13:37:16 +0200

    test

--- testphp/resultat-test.php
+++ testphp/resultat-test.php
@@ -26,26 +26,26 @@
 </table>
 <br>
 <table border="5"> 
-<?php
-echo "<caption>Table d'addition en base $nombre</caption>"; // Titre du tableau
-echo "<tr><th>+</th>"; // Cellule haut gauche
+    <?php
+        echo "<caption>Table d'addition en base $nombre</caption>"; // Titre du tableau
+        echo "<tr><th>+</th>"; // Cellule haut gauche
 
-for($i=0;$i<$nombre;$i++)
-	echo "<th>$i</th>";
+        for($i=0;$i<$nombre;$i++)
+            echo "<th>$i</th>";
 
-echo "</tr>\n";
+        echo "</tr>\n";
 
-for($j=0;$j<$nombre;$j++)
-{
- 	echo "<tr><th>$j</th>";
+        for($j=0;$j<$nombre;$j++)
+        {
+            echo "<tr><th>$j</th>";
 
-	for($i=0;$i<$nombre;$i++)
-	{
-		 echo "<td>".base_convert($j+$i, 10, $nombre)."</td>"; // Affichage dans les cellules + convertir
-	}
-	
-	echo "</tr>";
- }
-?>
+            for($i=0;$i<$nombre;$i++)
+            {
+                 echo "<td>".base_convert($j+$i, 10, $nombre)."</td>"; // Affichage dans les cellules 
+            }
+            
+            echo "</tr>";
+         }
+    ?>
 </table>
 
